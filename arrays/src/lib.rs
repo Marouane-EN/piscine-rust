82d5c342896cdd4ba54b6ea2b26bc3ffb95ba32d
Marouane <enmaroine443@gmail.com>
2026-01-06 17:42:41 +0100

    quest 3 exercice 4

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -1,10 +1,9 @@
-pub fn sum(a: &[i32; 10]) -> i32 {
+pub fn sum(a: &[i32]) -> i32 {
     a.iter().sum()
 }
 
 pub fn thirtytwo_tens() -> [i32; 32] {
-    let arr: [i32; 32] = std::array::from_fn(|_| 10);
-    arr
+    [10; 32]
 }
 
 #[cfg(test)]
@@ -15,7 +14,7 @@ mod tests {
     fn it_works() {
         let a: [i32; 10] = (1..=10).collect::<Vec<i32>>().try_into().unwrap();
         let b = [5; 10];
-        assert_eq!(sum(&a), 55);
+        assert_eq!(sum((1..=10).collect::<Vec<_>>().as_slice()), 55);
         assert_eq!(sum(&b), 50);
         assert_eq!(thirtytwo_tens().len(), 32);
         assert_eq!(
