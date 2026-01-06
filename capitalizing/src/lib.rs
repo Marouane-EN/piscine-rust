10bdc1a05e951ade4106745874759a241140b45c
Marouane <enmaroine443@gmail.com>
2026-01-06 19:06:26 +0100

    quest 3 exercice 6

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -11,15 +11,21 @@ pub fn capitalize_first(input: &str) -> String {
 }
 
 pub fn title_case(input: &str) -> String {
-    let mut words = input.split_ascii_whitespace().map(String::from).collect::<Vec<String>>();
+    let mut words = input
+        .split_inclusive(|c: char| c == ' ')
+        .map(String::from)
+        .collect::<Vec<String>>();
     for word in words.iter_mut() {
         *word = word.chars().nth(0).unwrap().to_ascii_uppercase().to_string() + &word[1..];
     }
-    words.join(" ")
+    words.join("")
 }
 
 pub fn change_case(input: &str) -> String {
-    let mut words = input.split_ascii_whitespace().map(String::from).collect::<Vec<String>>();
+    let mut words = input
+        .split_inclusive(|c: char| c == ' ')
+        .map(String::from)
+        .collect::<Vec<String>>();
     for word in words.iter_mut() {
         *word = word
             .chars()
@@ -34,7 +40,7 @@ pub fn change_case(input: &str) -> String {
             })
             .collect();
     }
-    words.join(" ")
+    words.join("")
 }
 
 #[cfg(test)]
@@ -43,6 +49,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(capitalize_first("1\ts"), "Joe is missing");
+        assert_eq!(title_case("Hello My\t\tName Is Carl"), "Hello My\t\tName Is Carl");
     }
 }
