43ebf23e3df1f384b4a88b9a1b0e28976e9819d3
Marouane <enmaroine443@gmail.com>
2026-01-07 14:33:41 +0100

    quest 3 exercice 7

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -2,17 +2,17 @@ use std::cmp;
 pub fn edit_distance(source: &str, target: &str) -> usize {
     let m = source.len(); // 10
     let n = target.len(); // 10 
-    let mut matrix = vec![vec![0;n];m]; // [ []10 ]10
+    let mut matrix = vec![vec![0;n+1];m+1]; // [ []10 ]10
 
-    for i in 0..n {
+    for i in 0..=n {
         matrix[0][i] = i;
     }
-    for j in 0..m {
+    for j in 0..=m {
         matrix[j][0] = j;
     }
-    for row in 1..m {
-        for col in 1..n {
-            if source.chars().nth(row) == target.chars().nth(col) {
+    for row in 1..=m {
+        for col in 1..=n {
+            if source.chars().nth(row-1) == target.chars().nth(col-1) {
                 matrix[row][col] = matrix[row - 1][col - 1];
             } else {
                 matrix[row][col] =
@@ -25,7 +25,7 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
         }
     }
 
-    matrix[m-1][n-1]
+    matrix[m][n]
 }
 #[cfg(test)]
 mod tests {
@@ -33,8 +33,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let source = "alignment";
-        let target = "assignment";
-        assert_eq!(edit_distance(source, target), 2);
+        let source = "hourse";
+        let target = "rous";
+        assert_eq!(edit_distance(source, target), 3);
     }
 }
