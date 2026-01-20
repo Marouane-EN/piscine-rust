02b10633a39123f9a5be64b5f8d3f51ca6038f8b
Marouane <enmaroine443@gmail.com>
2026-01-20 15:38:53 +0100

    quest 7 exercice 5

--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -14,17 +14,12 @@ impl Tracker {
     pub fn set_value<T>(&self, v: &Rc<T>) {
         let n = Rc::strong_count(v);
         let p = (n * 100) / self.max;
-        if p > 70 {
+        if n > self.max {
+            self.messages.borrow_mut().push(format!("Error: You can't go over your quota!"));
+        } else if p >= 70 {
             self.messages
                 .borrow_mut()
-                .push(
-                    format!(
-                        "Warning: You have used up over {}% of your quota!",
-                        (n * 100) / self.max
-                    )
-                );
-        } else if n >= self.max {
-            self.messages.borrow_mut().push(format!("Error: You can't go over your quota!"));
+                .push(format!("Warning: You have used up over {}% of your quota!", p));
         }
     }
 
@@ -32,8 +27,13 @@ impl Tracker {
         let n = Rc::strong_count(v);
         self.messages
             .borrow_mut()
-            .push(
-                format!("Info: This value would use {}% of your quota", (n * 100) / self.max)
-            )
+            .push(format!("Info: This value would use {}% of your quota", (n * 100) / self.max))
     }
 }
+/*
+thread 'test_one' (98) panicked at src/lib.rs:27:5:
+assertion `left == right` failed
+  left: ["Info: This value would use 40% of your quota", "Warning: You have used up over 80% of your quota!", "Warning: You have used up over 100% of your quota!", "Warning: You have used up over 120% of your quota!"]
+ right: ["Info: This value would use 40% of your quota", "Warning: You have used up over 80% of your quota!", "Warning: You have used up over 100% of your quota!", "Error: You can't go over your quota!"]
+
+*/
