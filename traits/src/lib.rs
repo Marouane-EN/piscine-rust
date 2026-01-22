44a727c7a0098d0b2af18a0400a60b3b3f96c4bc
Marouane <enmaroine443@gmail.com>
2026-01-22 16:45:15 +0100

    quest 8 exercice 4

---
+++ traits/src/lib.rs
@@ -0,0 +1,65 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Player<'a> {
+    pub name: &'a str,
+    pub strength: f64,
+    pub score: u32,
+    pub money: u32,
+    pub weapons: Vec<&'a str>,
+}
+
+pub struct Fruit {
+    pub weight_in_kg: f64,
+}
+
+pub struct Meat {
+    pub weight_in_kg: f64,
+    pub fat_content: f64,
+}
+
+impl Player<'_> {
+    pub fn eat(&mut self, food: impl Food) {
+        self.strength += food.gives();
+    }
+}
+
+impl fmt::Display for Player<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{}\nStrength: {}, Score: {}, Money: {}\nWeapons: {:?}",
+            self.name,
+            self.strength,
+            self.score,
+            self.money,
+            self.weapons
+        )
+    }
+}
+
+pub trait Food {
+    fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    fn gives(&self) -> f64 {
+        4.0 * self.weight_in_kg.ceil()
+    }
+}
+
+impl Food for Meat {
+    fn gives(&self) -> f64 {
+        (self.weight_in_kg - self.fat_content).ceil() * 4.0 + self.fat_content.ceil() * 9.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+
+    }
+}
