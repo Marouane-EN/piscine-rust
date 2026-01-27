fea3ae82498125f247bfc796a0b4709f8157f8e2
Marouane <enmaroine443@gmail.com>
2026-01-27 12:32:32 +0100

    quest 9 exercice 1

--- lalgebra_scalar/src/lib.rs
+++ lalgebra_scalar/src/lib.rs
@@ -7,65 +7,65 @@ pub trait Scalar: Add<Output = Self> +
     Copy
 {
     type Item;
-    fn zero() -> Self::Item;
-    fn one() -> Self::Item;
+    fn zero() -> Self;
+    fn one() -> Self;
 }
 
 impl Scalar for u32 {
     type Item = u32;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1
     }
 }
 impl Scalar for u64 {
     type Item = u64;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1
     }
 }
 
 impl Scalar for i32 {
     type Item = i32;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1
     }
 }
 
 impl Scalar for i64 {
     type Item = i64;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1
     }
 }
 
 impl Scalar for f32 {
     type Item = f32;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0.0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1.0
     }
 }
 
 impl Scalar for f64 {
     type Item = f64;
-    fn zero() -> Self::Item {
+    fn zero() -> Self {
         0.0
     }
-    fn one() -> Self::Item {
+    fn one() -> Self {
         1.0
     }
 }
