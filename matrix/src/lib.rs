0fe1172f382fb380664bbfacc8957e3cacd8ecaa
Marouane <enmaroine443@gmail.com>
2026-01-27 12:13:59 +0100

    quest 9 exercice 1

---
+++ matrix/src/lib.rs
@@ -0,0 +1,36 @@
+use scalar::Scalar;
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct Matrix<const W: usize, const H: usize, T: Scalar>(pub [[T; W]; H]);
+
+impl<const W: usize, const H: usize, T: Scalar> Matrix<W, H, T> {
+    pub fn zero() -> Self {
+        Self([[T::zero(); w]; H])
+    }
+}
+
+impl<const S: usize, T: Scalar> Matrix<S, S, T> {
+    pub fn identity() -> Self {
+        let mut raw = [[T::zero(); S]; S];
+        for i in 0..S {
+            raw[i][i] = T::one();
+        }
+
+        Matrix(raw)
+    }
+}
+
+impl<S, T> Matrix<S, S, T> {
+    pub fn identity() -> Self {
+        todo!()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
