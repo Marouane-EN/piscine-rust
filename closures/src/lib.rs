609767b603a021b4cadc91775fb937137bd3298e
Marouane <enmaroine443@gmail.com>
2026-01-27 17:12:09 +0100

    quest 10 exercice 1

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
     let mut res = Vec::new();
     let mut i = 2;
-    while res.len() <= 50 {
+    while res.len() < 50 {
         let j = i * i;
         if j % 2 == 0 {
             res.push(j);
