e09873eab17721f3d5fcdabfa2dd314a3c4ebe70
Marouane <enmaroine443@gmail.com>
2026-01-28 10:39:19 +0100

    quest 10 exercice 2

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -23,55 +23,26 @@ impl Cart {
         }
     }
     pub fn generate_receipt(&mut self) -> Vec<f32> {
-        self.items.sort_by(|a, b| a.1.total_cmp(&b.1));
-        let sum: f32 = self.items
+        let mut prices: Vec<f32> = self.items
             .iter()
-            .map(|f| f.1)
-            .sum();
+            .map(|p| p.1)
+            .collect();
 
-        let mut sub = 0.0;
+        prices.sort_by(|a, b| a.total_cmp(b));
 
-        if self.items.len() >= 3 {
-            sub = self.items[0].1 / sum;
-        }
+        let fm = prices.iter().take(prices.len() / 3);
+        let free_sum: f32 = fm.sum();
 
-        if self.items.len() >= 9 {
-            sub = self.items[0].1 + self.items[1].1 + self.items[2].1 / sum;
-        }
+        let total_sum: f32 = prices.iter().sum();
 
-        for item in self.items.iter() {
-            let mut i = item.1 * (1.0 - sub);
-            i = (i * 100.0).round() / 100.0;
-            self.receipt.push(i);
-        }
+        self.receipt = prices
+            .iter()
+            .map(|val| {
+                let discounted = (val * (total_sum - free_sum)) / total_sum;
+                (discounted * 100.0).round() / 100.0
+            })
+            .collect();
 
         self.receipt.clone()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let store = Store::new(
-            vec![
-                (String::from("product A"), 1.23),
-                (String::from("product B"), 23.1),
-                (String::from("product C"), 3.12)
-            ]
-        );
-
-        println!("{:?}", store);
-
-        let mut cart = Cart::new();
-        cart.insert_item(&store, String::from("product A"));
-        cart.insert_item(&store, String::from("product B"));
-        cart.insert_item(&store, String::from("product C"));
-
-        println!("{:?}", cart.generate_receipt());
-
-        println!("{:?}", cart);
     }
 }
